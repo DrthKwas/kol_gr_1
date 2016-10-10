1ef62ab4b7d255457685da6eec4660f78a75e17c
DrthKwas <2piwnik@stud114>
2016-10-10 18:30:48 +0200

    add simulator class

--- sym.py
+++ sym.py
@@ -1,3 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
+import random
+import time
 
+class Simulator:
+    def __init__(self):
+        self.angle = 0
+        
+    def run(self):
+        while True:
+            self.angle = self.angle + self.stder()
+            self.correction = self.corect()
+            self.printData()
+            
+            time.sleep(1)
+            
+    def corect(self):
+        return 0.66 * self.angle 
+        
+    def printData(self):
+        print("\nAngle: " + str(self.angle))
+        print("Correction: " + str(self.correction))
+        
+    def stder(self):
+        return random.gauss(-1,1)
+        
+sim = Simulator()
+sim.run()
